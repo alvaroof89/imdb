5a3357811af8964b81559b6bdac5781c76b312e1
alvaroof89 <alvaroof89@gmail.com>
2016-08-22 11:43:45 +0200

    finished script - v1
    now the script is fully functional and created a csv that can be loaded into R

--- loading-plots.py
+++ loading-plots.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Aug 22 09:14:32 2016
-
 @author: alvaroof89
+Script for loading, parsing, and then exporting into csv the plot.list file rom imdb database
+
+OUTPUT: key:title value:plot
+
 """
 import re
+import csv
 
 #import the plot.list file from the imdb database and parse it using regexp module
 file = open("C:/Users/GGORS/Desktop/Data Science/plot.list", 'r')
@@ -15,4 +19,11 @@ for i in range(0,len(p)):
     dict[str(p[i][0])] = re.sub("(\nPL:|\n|PL: )","",str(p[i][1]))
 
 #Now 'dict' has key:title and value:plot. Ready for analyse
+#Export to csv so it can be easily loaded into R afterwards
 
+with open('C:/Users/GGORS/Desktop/Data Science/dict.csv', 'wb') as csv_file:
+    writer = csv.writer(csv_file)
+    for key, value in dict.items():
+       writer.writerow([key, value])
+       
+#the text mining procedures will be performed in R scripts kept in this same project folder
